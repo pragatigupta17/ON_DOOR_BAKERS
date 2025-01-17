e788445c4c0fe056a759812bcdc07d94e6179280
Pragati Gupta <pragatigupta0104@gmail.com>
2025-01-18 00:20:59 +0530

    push

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function addtocart(e){
+    let a =document.querySelector('#container');
+    
+ 
+        a.style.display="block";
+     
+    
+    
+}
+function closeAddtocart(e){
+    let a =document.querySelector('#container');
+    a.style.display="none"
+}
+
